a8aafc30d6dda97ad8031aa199b5911ead973f32
Scarlett <scarlett@vynyl.com>
2018-09-22 11:01:21 -0600

    set up image recognition

--- facialRec/js/HelloWorldSceneAR.js
+++ facialRec/js/HelloWorldSceneAR.js
@@ -1,47 +1,143 @@
-'use strict';
 
 import React, { Component } from 'react';
-
-import {StyleSheet} from 'react-native';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
 import {
   ViroARScene,
-  ViroText,
-  ViroConstants,
+  ViroImage,
+  ViroQuad,
+  ViroNode,
+  ViroMaterials,
+  ViroOmniLight,
+  ViroARTrackingTargets,
+  ViroARImageMarker,
+  ViroAnimations,
+  Viro3DObject,
+  ViroSpotLight,
+  ViroAmbientLight,
+  ViroParticleEmitter,
+  ViroSphere,
 } from 'react-viro';
 
-export default class HelloWorldSceneAR extends Component {
+var createReactClass = require('create-react-class');
 
-  constructor() {
-    super();
-
-    // Set initial state here
-    this.state = {
-      text : "Initializing AR..."
+var ARPosterDemo = createReactClass({
+  getInitialState: function() {
+    return {
+      loopState:false,
+      animationName:"01",
+      pauseUpdates : false,
+      playAnim: false,
+      modelAnim: false,
     };
-
-    // bind 'this' to functions
-    this._onInitialized = this._onInitialized.bind(this);
-  }
+  },
 
   render() {
     return (
-      <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+      <ViroARScene>
+        <ViroAmbientLight color="#ffffff" intensity={200}/>
+
+        // IMAGE MARKER 1
+        <ViroARImageMarker target={"kanyeAlbum"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
+
+          <ViroNode position={[0, -.1, 0]} scale={[0,0,0]} rotation={[-90, 0, 0]} dragType="FixedToWorld" onDrag={()=>{}}
+            animation={{name:"scaleModel", run:this.state.playAnim,}} >
+            <Viro3DObject onLoadEnd={this._onModelLoad}
+              source={require('./res/blackpanther/object_bpanther_anim.vrx')}
+              resources={[require('./res/blackpanther/object_bpanther_Base_Color.png'),
+                          require('./res/blackpanther/object_bpanther_Metallic.png'),
+                          require('./res/blackpanther/object_bpanther_Mixed_AO.png'),
+                          require('./res/blackpanther/object_bpanther_Normal_OpenGL.png'),
+                          require('./res/blackpanther/object_bpanther_Roughness.png')]}
+              position={[0, -1.45, 0]}
+              scale={[.9,.9,.9]}
+              animation={{name:this.state.animationName, run:this.state.modelAnim, loop:this.state.loopState, onFinish:this._onFinish,}}
+              type="VRX" />
+
+          </ViroNode>
+
+        </ViroARImageMarker>
+
+
+        <ViroOmniLight
+            intensity={300}
+            position={[-10, 10, 1]}
+            color={"#FFFFFF"}
+            attenuationStartDistance={20}
+            attenuationEndDistance={30} />
+
+        <ViroOmniLight
+            intensity={300}
+            position={[10, 10, 1]}
+            color={"#FFFFFF"}
+            attenuationStartDistance={20}
+            attenuationEndDistance={30} />
+
+        <ViroOmniLight
+            intensity={300}
+            position={[-10, -10, 1]}
+            color={"#FFFFFF"}
+            attenuationStartDistance={20}
+            attenuationEndDistance={30} />
+
+        <ViroOmniLight
+            intensity={300}
+            position={[10, -10, 1]}
+            color={"#FFFFFF"}
+            attenuationStartDistance={20}
+            attenuationEndDistance={30} />
+
+        <ViroSpotLight
+          position={[0, 8, -2]}
+          color="#ffffff"
+          direction={[0, -1, 0]}
+          intensity={50}
+          attenuationStartDistance={5}
+          attenuationEndDistance={10}
+          innerAngle={5}
+          outerAngle={20}
+          castsShadow={true}
+        />
+
+        <ViroQuad
+          rotation={[-90, 0, 0]}
+          position={[0, -1.6, 0]}
+          width={5} height={5}
+          arShadowReceiver={true}
+          />
+
       </ViroARScene>
     );
-  }
+  },
 
-  _onInitialized(state, reason) {
-    if (state == ViroConstants.TRACKING_NORMAL) {
+  _onFinish(){
+    this.setState({
+      animationName : "02",
+      loopState: true,
+    })
+  },
+
+  _onAnchorFound() {
+    this.setState({
+      pauseUpdates: true,
+      playAnim: true,
+      modelAnim: true,
+    })
+  },
+
+  _onModelLoad() {
+    setTimeout(()=> {
       this.setState({
-        text : "Hello World!"
-      });
-    } else if (state == ViroConstants.TRACKING_NONE) {
-      // Handle loss of tracking
-    }
-  }
-}
+
+      })
+    }, 3000);
+  },
+});
 
 var styles = StyleSheet.create({
   helloWorldTextStyle: {
@@ -49,8 +145,26 @@ var styles = StyleSheet.create({
     fontSize: 30,
     color: '#ffffff',
     textAlignVertical: 'center',
-    textAlign: 'center',  
+    textAlign: 'center',
   },
 });
 
-module.exports = HelloWorldSceneAR;
+ViroARTrackingTargets.createTargets({
+  kanyeAlbum : {
+    source : require('./res/kanye.jpeg'),
+    orientation : "Up",
+    physicalWidth : 0.6096 // real world width in meters
+  },
+  gagaAlbum : {
+    source : require('./res/ladygaga.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.6096 // real world width in meters
+  }
+});
+
+ViroAnimations.registerAnimations({
+    scaleModel:{properties:{scaleX:1, scaleY:1, scaleZ:1,},
+                  duration: 1000},
+});
+
+module.exports = ARPosterDemo;
